7a81249a524205e810178b36aa48ffbfec7a064d
Anastacy Slyusar <ana.milpalm@gmail.com>
2024-10-30 12:03:20 +0100

    tasks were added till #14

--- Задачи мои/webdio/test/specs/test.e2e.js
+++ Задачи мои/webdio/test/specs/test.e2e.js
@@ -85,7 +85,7 @@ xit("should show getText command", async () => {
  console.log("Subtitle text is: "+ await subtitle.getText())
 })
 
-it("should show url command", async () => {
+xit("should show url command", async () => {
     await browser.url(`https://webdriver.io`);
 
    const blogButton = await $('.button[href="/docs/gettingstarted"]')
@@ -93,7 +93,7 @@ it("should show url command", async () => {
    console.log("Is clickable: " + clickable) //outputs true
 });
 
-it("should show if an element is displayed", async () => {
+xit("should show if an element is displayed", async () => {
     await browser.url(`https://webdriver.io`);
 
     const blogButton = await $('.button[href="/docs/gettingstarted"]')
@@ -101,7 +101,7 @@ it("should show if an element is displayed", async () => {
     console.log("🚀 ~ it ~ displayed:" + displayed)
 });
 
-it("should show if an element is visible", async () => {
+xit("should show if an element is visible", async () => {
     await browser.url(`https://webdriver.io`)
 
     const blogButton = await $('.burron[href="/docs/gettingstarted"]')
@@ -109,5 +109,71 @@ it("should show if an element is visible", async () => {
     console.log("🚀 ~ it ~ displayedInViewport: " + displayedInViewport)
 });
 
+xit("should show if an element is enabled", async () => {
+    await browser.url(`https://webdriver.io`);
+
+    const gettingstartedButton = await $('button[href="/docs/gettingstarted"]')
+
+    let isEnabled = await gettingstartedButton.isEnabled()
+    console.log("Is get started button enabled: " + isEnabled)  
+});
+
+xit("should show if an element is enabled", async () => {
+    await browser.url(`https://webdriver.io`);
+
+    const gettingstartedButton = await $('.button[href="/docs/gettingstarted"]')
+
+    let isFocused = await gettingstartedButton.isFocused()
+    console.log("Is get started button focused before click: " + isFocused)  
+    await browser.pause(2000)
+    await getStartedButton.click()
+    console.log("Is get started button focused after click: " + isFocused)
+    await browser.pause(2000)
+});
+
+xit("should show movement to element action", async () => {
+    await browser.url(`https://webdriver.io`);
+
+    const getStartedLink = await $('.footer__link-item[href="/docs/gettingstarted"]')
+
+    await browser.pause(2000)
+    await getStartedLink.scrollIntoView()
+   await browser.pause(2000)
+});
+
+xit("should show waitUntil coomand", async () => {
+    await browser.url(`https://webdriver.io/`)
+
+    await browser.waitUntil(async () => {
+        return $('.button[href="/docs/gettingstarted"]').isDisplayed();
+    }, 5000, "Button is not displayed")
+});
+
+xit("should get html for certain elements", async () => {
+    await browser.url(`https://webdriver.io`);
+
+    const outerHTML = await $('.dropdown__menu').getHTML()
+    console.log("outerHTML :" + outerHTML)
+
+    const innerHTML = await $('.dropdown__menu').getHTML(false)
+    console.log("innerHTML :" + innerHTML)
+});
+
+it("should show save screenshot command", async () => {
+    await browser.url(`https://webdriver.io`);
+
+    const getStartedLink = await $('.footer__link-item[href="/docs/gettingstarted"]')
+    await browser.pause(2000)
+    await getStartedLink.saveScreenshot('linkScreenshot.png')
+});
+it("should switch to another window", async () => {
+    await browser.url(`https://webdriver.io`);
+
+    await browser.newWindow(`https://webdriver.io`);
+    await browser.pause(2000)
+
+    await browser.switchWindow(`https://webdriver.io`);
+    await browser.pause(2000)
+});
 });
 
